ff9bca590539adc4114cad5e4dcfe807ac6b0aeb
Nikhil Raj <114593761+nik5526@users.noreply.github.com>
2026-08-29 00:33:02 +0530

    Time: 3 ms (64.55%), Space: 8.1 MB (20.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
+    private:
+    int f(int n,vector<int>& dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
+    public:
     int fib(int n) {
-
-    if(n==0){
-        return 0;
-    }
-    if(n==1){
-        return 1;
-    }
-
-    return (fib(n-1)+fib(n-2));
-
+        vector<int> dp(n+1,-1);
+        int ans=f(n,dp);
+        return ans;
     }
 };
