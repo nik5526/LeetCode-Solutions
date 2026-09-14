3d559936845a78500f0a923693c2d160e40d2931
Nikhil Raj <nikhilrajy909@gmail.com>
2026-09-14 11:52:34 +0530

    Time: 35 ms (13.06%), Space: 45.3 MB (9.48%) - LeetHub - Time: O(2^n), Space: O(n)

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void comb(vector<int>& candidates, int target,int n, int idx,vector<int> diary,vector<vector<int>> &ans, int sum){
+        if(idx == n){
+            if(sum == target){
+                ans.push_back(diary);
+            }
+            return;
+        }
+        comb(candidates, target, n, idx+1, diary, ans, sum);
+        if(candidates[idx] + sum<=target){
+            diary.push_back(candidates[idx]);
+            sum = sum + candidates[idx];
+            comb(candidates, target, n, idx, diary, ans, sum);
+            diary.pop_back();
+            sum = sum - candidates[idx];
+        }
+
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        int idx = 0;
+        vector<int> diary;
+        vector<vector<int>> ans;
+        int sum = 0;
+        comb(candidates, target, n, idx, diary, ans, sum);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
